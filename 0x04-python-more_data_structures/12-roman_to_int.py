0496fe59a34e01ffcd1f9148a0d469598b757cc4
30501 <101524548+30501@users.noreply.github.com>
2022-07-06 23:00:15 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@
 
 def roman_to_int(roman_string):
     """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
+    if (not (roman_string, str) or
             roman_string is None):
         return (0)
 
