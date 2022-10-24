00e261f360f9be75469067ee78d4b960412e578b
30501 <101524548+30501@users.noreply.github.com>
2022-10-24 23:20:21 +0300

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-""" POST and Search API """
-import requests
-from sys import argv
+"""Sends a request to the URL and displays the body of the response."""
 
 
-if __name__ == "__main__":
-    url = 'http://0.0.0.0:5000/search_user'
-    if (len(argv) > 1):
-        letter = argv[1]
-    else:
-        letter = ""
-    payload = {'q': letter}
-    req = requests.post(url, data=payload)
-    try:
-        json_body = req.json()
-        if (len(json_body) != 0):
-            print("[{}] {}".format(json_body['id'], json_body['name']))
+if __name__ == '__main__':
+    from requests import post
+    from sys import argv
+
+    URL = 'http://0.0.0.0:5000/search_user'
+    data = {'q': argv[1] if len(argv) >= 2 else ""}
+    response = post(URL, data)
+
+    type_res = response.headers['content-type']
+
+    if type_res == 'application/json':
+        result = response.json()
+        _id = result.get('id')
+        name = result.get('name')
+        if (result != {} and _id and name):
+            print("[{}] {}".format(_id, name))
         else:
-            print("No result")
-    except:
-        print("Not a valid JSON")
+            print('No result')
+    else:
+        print('Not a valid JSON')
